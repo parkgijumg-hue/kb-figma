2c9b3773d1eac13d8eb3d53a0bdfb26435e0488d
박기정 <parkgijumg@gmail.com>
2026-05-08 10:20:33 +0900

    render주소변경

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://ai-figma-4.onrender.com',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://kb-figma-t2qh.onrender.com',
   headers: {
     'Content-Type': 'application/json'
   }
